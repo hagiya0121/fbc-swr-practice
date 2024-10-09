188030152c88a43e4a98912a47ae757bf0a6d9e8
hagiya0121 <koazu0121@gmail.com>
2024-10-09 20:10:05 +0900

    feat: SWRでデータを取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
+import useSWR from "swr";
 import "./App.css";
 
 function App() {
-  const url = "https://httpstat.us/200?sleep=2000";
-  const headers = { Accept: "application/json" };
-
   const [status, setStatus] = useState("");
 
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
+  const fetcher = (url, headers) =>
+    fetch(url, { headers }).then((res) => res.json());
+
+  const url = "https://httpstat.us/200?sleep=2000";
+  const headers = { Accept: "application/json" };
+  const { error, isLoading } = useSWR(
+    [url, headers],
+    ([url, headers]) => fetcher(url, headers),
+    {
+      onSuccess: (data) => {
+        setStatus(data.description);
+      },
+    }
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Failed to load.</div>;
 
   return <>{status && <p>Status : {status}</p>}</>;
 }
