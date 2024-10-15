8f20447ed1960e37d1546dac2038210e4e4e7d6e
hagiya0121 <koazu0121@gmail.com>
2024-10-15 11:31:37 +0900

    refactor: useStateを使わずにdataを直接利用するように変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import { useState } from "react";
 import useSWR from "swr";
 import "./App.css";
 
 function App() {
-  const [status, setStatus] = useState("");
-
   const fetcher = (url, headers) =>
     fetch(url, { headers }).then((res) => res.json());
 
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
-  const { error, isLoading } = useSWR(
-    [url, headers],
-    ([url, headers]) => fetcher(url, headers),
-    {
-      onSuccess: (data) => {
-        setStatus(data.description);
-      },
-    }
+  const { data, error, isLoading } = useSWR([url, headers], ([url, headers]) =>
+    fetcher(url, headers)
   );
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Failed to load.</div>;
 
-  return <>{status && <p>Status : {status}</p>}</>;
+  return <>{data && <p>Status : {data.description}</p>}</>;
 }
 
 export default App;
